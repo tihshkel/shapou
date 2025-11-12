acfe7643be7e6915c00f3a93069a1e4cd04e8c0e
tihshkel <tihshkle@gmail.com>
2025-11-12 13:53:11 +0300

    Initial Commit

---
+++ ShaPou/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShaPou
+//
+//  Created by Tihon Shkel on 12.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShaPou/ShaPouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaPouApp.swift
+//  ShaPou
+//
+//  Created by Tihon Shkel on 12.11.25.
+//
+
+import SwiftUI
+
+@main
+struct ShaPouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
